c5e6ca11d3483614248c8224a8b37f50d8c84802
Aylén Bavosa Castro <aylucastro@gmail.com>
2021-02-10 20:16:12 -0300

    Agregado de código javascript

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Controla el paso de img (next y prev)
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Controla el paso de relojes por click en la img
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  console.log(slides);
+
+  if (n > slides.length) {slideIndex = 1;}
+  if (n < 1) {slideIndex = slides.length;}
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
+}
+
